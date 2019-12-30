37a8e2e133c107569d35c142cfa4d325c8332a77
AshutoshDKedar <ashutoshdkedar@gmail.com>
2019-12-30 16:01:53 +0530

    1 file modified

--- tabs.js
+++ tabs.js
@@ -12,7 +12,6 @@ import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
 import ThumbDown from '@material-ui/icons/ThumbDown';
 import ThumbUp from '@material-ui/icons/ThumbUp';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
